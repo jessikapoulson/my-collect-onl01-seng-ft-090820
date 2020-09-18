19b76603ab2a7ff514affc246ba59721471e4188
Jessi Poulson <jessipoulson@hotmail.com>
2020-09-18 16:36:45 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,13 +1,7 @@
 def my_collect(array)
   i=0
  collection =[]
- while i
-end
-
-def my_collect(array)
-  i = 0
-  collect = []
-  while i < array.length
+while i < array.length
     collect << yield(array[i])
     i+=1
   end
